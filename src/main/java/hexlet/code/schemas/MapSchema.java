e8bb94fd96ba87c858a55cc1732d1403d0208f04
m.balashov <m.balashov@crpt.ru>
2022-04-05 10:34:02 +0300

    fix "hexlet-checks"

--- src/main/java/hexlet/code/schemas/MapSchema.java
+++ src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package hexlet.code.schemas;
 
 
@@ -54,60 +53,3 @@ public final class MapSchema extends BaseSchema {
     }
 
 }
-=======
-package hexlet.code.schemas;
-
-
-import java.util.Map;
-import java.util.function.Predicate;
-
-public final class MapSchema extends BaseSchema {
-    public MapSchema() {
-        super();
-    }
-
-    public MapSchema required() {
-        Predicate<Object> requiredCheck = map -> ((map != null) && (map instanceof Map));
-        addChecks("required ", requiredCheck);
-        return this;
-    }
-
-    public MapSchema sizeOf(int size) {
-        Predicate<Object> sizeOfCheck = map -> ((Map) map).size() == size;
-        addChecks("sizeOf", sizeOfCheck);
-        return this;
-    }
-
-    public MapSchema shape(Map<String, BaseSchema> schemas) {
-
-        Predicate<Object> shapeChecks = map -> {
-            if (shapePreChecks(map, schemas)) {
-                return (shapeMajorChecks(map, schemas));
-            } else {
-                return false;
-            }
-        };
-        addChecks("shape", shapeChecks);
-        return this;
-    }
-
-    private boolean shapePreChecks(Object map, Map<String, BaseSchema> schemas) {
-        if (((Map) map).size() != schemas.size()) {
-            return false;
-        }
-        return schemas.entrySet().stream()
-                .allMatch(e -> ((Map) map).containsKey(e.getKey()));
-    }
-
-    private boolean shapeMajorChecks(Object map, Map<String, BaseSchema> schemas) {
-        for (String key : schemas.keySet()) {
-            Object o = ((Map) map).get(key);
-            if (!schemas.get(key).isValid(o)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-}
->>>>>>> fa90895 (ch logic of positive method)
